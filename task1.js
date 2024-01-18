e2fc0ba5aac7a66a8a61481221d7c93af21615bd
ojasvio <148864810+ojasvio@users.noreply.github.com>
2024-01-18 19:01:39 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,207 @@
+var number = 5; // in-line comment
+number = 9
+
+//data types and variables
+
+/* undefined, null, boolean, string, symbol, number, and object */
+var myName = "Ojasvi"
+myName = 8
+let ourName = "ieee embs"
+const pi = 3.14
+
+//storing values with the assigner
+var a;
+var b = 2;
+a = 7;
+b = a;
+console.log(a)
+
+//initializing variables
+var a = 9;
+
+//uninitialized variables
+/* initialize these three varaibles*/
+var a = 5;
+var b = 10;
+var c = "I am a";
+/*do not change code below this line*/
+a = a + 1;
+b = b + 5;
+c = c + "string! ";
+
+//case sensitivity in variables
+/*decleration*/
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+/*assignments*/
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+//adding numbers
+var sum = 10 + 10;
+console.log(sum)
+
+//subtracting numbers
+var difference = 45 - 33;
+
+//multiplying numbers
+var product = 8 * 0;
+
+//dividing numbers
+var quotient = 66 / 33;
+
+//incrementing numbers
+var myVar = 87;
+myVar = myVar + 1;
+/* myVar++; */
+
+//decrementing numbers
+var myVar = 11;
+myVar = myVar - 1;
+
+//decimal numbers
+var ourDecimal = 5.7;
+var myDecimal = 0.009;
+
+//multiply two decimals
+var product = 2.0 * 0.0;
+
+//divide decimals
+var quotient = 0.0 / 2.0;
+
+//finding a remainder
+var remainder;
+remainder = 11 % 3;
+
+//augmented math operations
+var a = 3;
+var b = 17;
+var c = 12;
+a = a + 12;
+b = 9 + b;
+c = c + 7;
+// a += 12;
+// b += 9;
+// c += 7;
+// a -= 12;
+// b -= 9;
+// c -= 7;
+
+//compound assignment with augmented multiplication
+a *= 12;
+b *= 9;
+c *= 7;
+
+//compund assignment with augmented division
+a /= 12;
+b /= 9;
+c /= 7;
+
+//declare string and variables
+var firstName = "Alan";
+var lastName = "Turing";
+
+//escaping literal quotes
+var myStr = "I am a \"double quotet\" string inside \"double quotes\"";
+console.log(myStr)
+
+//quoting strings with single quotes
+var myStr = "<a href="http://www.example.com" target="_blank">Link</a>";
+
+//escape sequences in strings
+/* CODE OUTPUT
+\' single quote
+\" double quote
+\\ backslash
+\n newline
+\r carriage return
+\t tab
+\b backspace
+\f form feed
+*/
+var ourStr = "I come first. " + "I come second. ";
+var myStr = "This is the start. "+"This is the end."
+console.log(myStr);
+ var ourStr = "I come first. ";
+ ourStr += "I come second.";
+ var myStr = "This is the first sentence. "
+ myStr += "This is the second sentence. "
+ console.log(myStr);
+
+//example
+var anAdjective = "awesome!";
+var ourStr = "freeCodeCamp is";
+ourStr += anAdjective;
+
+var someAdjective;
+var myStr = "Learning to code is ";
+
+//find length of string
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
